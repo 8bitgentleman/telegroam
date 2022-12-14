345a139d028d5e59109cfe5ac41aec278562fd55
Matt Vogel <mainmoniker@googlemail.com>
2022-12-13 20:35:35 -0500

    start shortcode panel

---
+++ src/components/ShortcodePanel.tsx
@@ -0,0 +1,159 @@
+import { Button, InputGroup, Intent, Label } from "@blueprintjs/core";
+import React, { useMemo, useState, useRef, useEffect } from "react";
+import MenuItemSelect from "roamjs-components/components/MenuItemSelect";
+import { getCleanCustomWorkflows } from "./core";
+import type { OnloadArgs } from "roamjs-components/types/native";
+
+const HotKeyEntry = ({
+  hotkey,
+  value,
+  order,
+  keys,
+  setKeys,
+  extensionAPI,
+  workflows,
+  workflowNamesByUid,
+}: {
+  hotkey: string;
+  value: string;
+  order: number;
+  keys: Record<string, string>;
+  setKeys: (r: Record<string, string>) => void;
+  extensionAPI: OnloadArgs["extensionAPI"];
+  workflows: { uid: string }[];
+  workflowNamesByUid: Record<string, string>;
+}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current.className = "rm-extensions-settings";
+    inputRef.current.style.minWidth = "100%";
+    inputRef.current.style.maxWidth = "100%";
+  }, [inputRef]);
+  return (
+    <div className={"flex items-center gap-1"}>
+      <Label className="flex-1">
+        Hot Key
+        <InputGroup
+          placeholder={"Type the keys themselves"}
+          value={hotkey}
+          onChange={() => true}
+          className={"w-full"}
+          inputRef={inputRef}
+          onKeyDown={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+            const parts = hotkey ? hotkey.split("+") : [];
+            const formatValue = (
+              e.key === "Backspace"
+                ? parts.slice(0, -1)
+                : ["Shift", "Control", "Alt", "Meta"].includes(e.key)
+                ? Array.from(new Set(parts.concat(e.key.toLowerCase()))).sort(
+                    (a, b) => b.length - a.length
+                  )
+                : parts.concat(e.key.toLowerCase())
+            ).join("+");
+            if (formatValue === hotkey) return;
+            const error = !formatValue || !!keys[formatValue];
+            const newKeys = Object.fromEntries(
+              Object.entries(keys).map((k, o) =>
+                o !== order ? k : [formatValue, k[1]]
+              )
+            );
+            setKeys(newKeys);
+            if (!error) {
+              extensionAPI.settings.set("hot-keys", newKeys);
+            }
+          }}
+          intent={Intent.NONE}
+        />
+      </Label>
+      <Label className={"flex-1"}>
+        SmartBlock
+        <MenuItemSelect
+          activeItem={value}
+          items={workflows.map((w) => w.uid)}
+          onItemSelect={(e) => {
+            const newKeys = Object.fromEntries(
+              Object.entries(keys).map((k, o) => (o !== order ? k : [k[0], e]))
+            );
+            setKeys(newKeys);
+            extensionAPI.settings.set("hot-keys", newKeys);
+          }}
+          transformItem={(e) => workflowNamesByUid[e]}
+          className={"w-full"}
+        />
+      </Label>
+      <Button
+        icon={"trash"}
+        style={{ width: 32, height: 32 }}
+        minimal
+        onClick={() => {
+          const newKeys = Object.fromEntries(
+            Object.entries(keys).filter((_, o) => o !== order)
+          );
+          setKeys(newKeys);
+          extensionAPI.settings.set("hot-keys", newKeys);
+        }}
+      />
+    </div>
+  );
+};
+
+const HotKeyPanel = (extensionAPI: OnloadArgs["extensionAPI"]) => () => {
+  const workflows = useMemo(
+    () =>
+      getCleanCustomWorkflows().sort(({ name: a }, { name: b }) =>
+        a.localeCompare(b)
+      ),
+    []
+  );
+  const workflowNamesByUid = useMemo(
+    () => Object.fromEntries(workflows.map((wf) => [wf.uid, wf.name])),
+    []
+  );
+  const [keys, setKeys] = useState(
+    () => extensionAPI.settings.get("hot-keys") as Record<string, string> || {}
+  );
+  return (
+    <div
+      style={{
+        width: "100%",
+        minWidth: 256,
+      }}
+    >
+      {Object.entries(keys).map(([key, value], order) => {
+        return (
+          <HotKeyEntry
+            key={order}
+            hotkey={key}
+            value={value}
+            order={order}
+            workflows={workflows}
+            workflowNamesByUid={workflowNamesByUid}
+            extensionAPI={extensionAPI}
+            keys={keys}
+            setKeys={setKeys}
+          />
+        );
+      })}
+      <Button
+        text={"Add Hot Key"}
+        intent={Intent.PRIMARY}
+        rightIcon={"plus"}
+        minimal
+        style={{ marginTop: 8 }}
+        onClick={async () => {
+          const randomWorkflow =
+            workflows[Math.floor(Math.random() * workflows.length)];
+          const newKeys = Object.fromEntries(
+            Object.entries(keys).concat([["control+o", randomWorkflow.uid]])
+          );
+          setKeys(newKeys);
+          extensionAPI.settings.set("hot-keys", newKeys);
+        }}
+      />
+    </div>
+  );
+};
+
+export default HotKeyPanel;
--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
-
-const panelConfig = {
-  tabTitle: "Test Ext 1",
-  settings: [
-    {id:		  "button-setting",
-     name:		"Button test",
-     description: "tests the button",
-     action:	  {type:	"button",
-             onClick: (evt) => { console.log("Button clicked!"); },
-             content: "Button"}},
-    {id:		  "switch-setting",
-     name:		"Switch Test",
-     description: "Test switch component",
-     action:	  {type:	 "switch",
-             onChange: (evt) => { console.log("Switch!", evt); }}},
-    {id:	 "input-setting",
-     name:   "Input test",
-     action: {type:		"input",
-          placeholder: "placeholder",
-          onChange:	(evt) => { console.log("Input Changed!", evt); }}},
-    {id:	 "select-setting",
-     name:   "Select test",
-     action: {type:	 "select",
-          items:	["one", "two", "three"],
-          onChange: (evt) => { console.log("Select Changed!", evt); }}},
-    // {id:	 "reactComponent-setting",
-    //  name:   "reactComponent test",
-    //  action: {type:	 "reactComponent",
-    //       component: reactButton}}
-  ]
-};
+import HotKeyPanel from "./components/ShortcodePanel";
 
 async function onload({extensionAPI}) {
 
   // set defaults if they dont' exist
-  if (!extensionAPI.settings.get('data')) {
-    await extensionAPI.settings.set('data', "01");
-  }
+  const panelConfig = {
+    tabTitle: "Flag Block",
+    settings: [
+        {id:     "input-setting",
+         name:   "Flag icon",
+         action: {type:        "input",
+                  placeholder: "⚑ ",
+                  onChange:    (evt) => { console.log("Input Changed!", evt); }}},
+        {id:     "select-setting",
+         name:   "Select test",
+         action: {type:     "select",
+                  items:    ["one", "two", "three"],
+                  onChange: (evt) => { console.log("Select Changed!", evt); }}},
+        {
+          id: "hot-keys",
+          name: "Hot Keys",
+          description:
+            "Set a custom hot key to flag the block",
+          action: {
+            type: "reactComponent",
+            component: HotKeyPanel(extensionAPI),
+          },
+        },
+    ]
+  };
   extensionAPI.settings.panel.create(panelConfig);
 
   console.log("load telegroam plugin");
