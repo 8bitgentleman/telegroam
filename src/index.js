5cb0864399a078e3fe357f13e47d7ade836b614a
Matt Vogel <mainmoniker@googlemail.com>
2022-12-13 20:18:51 -0500

    ignore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import toConfigPageName from "roamjs-components/util/toConfigPageName";
-import runExtension from "roamjs-components/util/runExtension";
-import { createConfigObserver } from "roamjs-components/components/ConfigPage";
 
-const extensionId = "telegroam";
-const CONFIG = toConfigPageName(extensionId);
-export default runExtension({
-  extensionId, 
-  run: () => {
-    createConfigObserver({ title: CONFIG, config: { tabs: [] } });
-  },
-  unload: () => {},
-});
+const panelConfig = {
+  tabTitle: "Test Ext 1",
+  settings: [
+    {id:		  "button-setting",
+     name:		"Button test",
+     description: "tests the button",
+     action:	  {type:	"button",
+             onClick: (evt) => { console.log("Button clicked!"); },
+             content: "Button"}},
+    {id:		  "switch-setting",
+     name:		"Switch Test",
+     description: "Test switch component",
+     action:	  {type:	 "switch",
+             onChange: (evt) => { console.log("Switch!", evt); }}},
+    {id:	 "input-setting",
+     name:   "Input test",
+     action: {type:		"input",
+          placeholder: "placeholder",
+          onChange:	(evt) => { console.log("Input Changed!", evt); }}},
+    {id:	 "select-setting",
+     name:   "Select test",
+     action: {type:	 "select",
+          items:	["one", "two", "three"],
+          onChange: (evt) => { console.log("Select Changed!", evt); }}},
+    // {id:	 "reactComponent-setting",
+    //  name:   "reactComponent test",
+    //  action: {type:	 "reactComponent",
+    //       component: reactButton}}
+  ]
+};
+
+async function onload({extensionAPI}) {
+
+  // set defaults if they dont' exist
+  if (!extensionAPI.settings.get('data')) {
+    await extensionAPI.settings.set('data', "01");
+  }
+  extensionAPI.settings.panel.create(panelConfig);
+
+  console.log("load telegroam plugin");
+}
+
+function onunload() {
+  console.log("unload telegroam plugin");
+}
   
+export default {
+onload,
+onunload
+};
